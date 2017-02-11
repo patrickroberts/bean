f6dcb4b88d4d8b1a845add19c4546d0282bfa88c
Patrick Roberts <pat2060@sbcglobal.net>
2017-02-11 01:51:15 -0600

    Added command-line utility

---
+++ bin/bean.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var bean = require('../dst/bean.min');
+
+if (process.argv.length < 3 || process.argv.length > 4) {
+  process.exit(1);
+}
+
+var chunks = [];
+
+process.stdin.on('data', function (chunk) {
+  chunks.push(chunk);
+});
+
+process.stdin.on('end', function () {
+  function isFlag(flag, value) {
+    var concise = '-' + flag.charAt(0);
+    var verbose = '--' + flag.charAt(0).toLowerCase() + flag.substr(1).replace(/[A-Z]/g, '-$&');
+
+    return value === concise || value === verbose;
+  }
+
+  function fromXxd(input) {
+    var output = '';
+    var line = /^[\da-f]{8}: ((?:[\da-f]{2} ?){0,16}) {2,}(?:.{16})$/gm;
+    var match, index, bytes;
+
+    while ((match = line.exec(input)) !== null) {
+      bytes = match[1].match(/[\da-f]{2}/g);
+
+      for (index = 0; index < bytes.length; index++) {
+        output += String.fromCharCode(parseInt(bytes[index], 16));
+      }
+    }
+
+    return output;
+  }
+
+  var filepath = path.resolve(process.cwd(), process.argv.pop());
+  var filetype = process.argv.length === 3 ? process.argv.pop() : '-r';
+  var input = Buffer.concat(chunks).toString('utf8');
+
+  var source;
+
+  if (isFlag('raw', filetype)) {
+    source = fs.readFileSync(filepath, 'binary');
+  } else if (isFlag('dump', filetype)) {
+    source = fromXxd(fs.readFileSync(filepath, 'utf-8'));
+  } else {
+    return process.exit(1);
+  }
+
+  console.log(bean.program(source)(input));
+});
